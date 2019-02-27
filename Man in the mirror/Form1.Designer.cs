642dd38b3276727f364c18bc2ece4368aafb9427
Deus <rogerferraro256@gmail.com>
2019-02-26 23:31:27 -0300

    first commit

---
+++ Man in the mirror/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Man_in_the_mirror
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImage = new System.Windows.Forms.Button();
+            this.ofdImage = new System.Windows.Forms.OpenFileDialog();
+            this.imgTogo = new System.Windows.Forms.PictureBox();
+            this.btnRar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDoIt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ofdRar = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgTogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(62, 33);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(191, 64);
+            this.btnImage.TabIndex = 0;
+            this.btnImage.Text = "Choose Image";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ofdImage
+            // 
+            this.ofdImage.FileName = "ofdImage";
+            // 
+            // imgTogo
+            // 
+            this.imgTogo.Location = new System.Drawing.Point(51, 128);
+            this.imgTogo.Name = "imgTogo";
+            this.imgTogo.Size = new System.Drawing.Size(231, 224);
+            this.imgTogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgTogo.TabIndex = 1;
+            this.imgTogo.TabStop = false;
+            // 
+            // btnRar
+            // 
+            this.btnRar.Location = new System.Drawing.Point(530, 33);
+            this.btnRar.Name = "btnRar";
+            this.btnRar.Size = new System.Drawing.Size(191, 64);
+            this.btnRar.TabIndex = 2;
+            this.btnRar.Text = "Choose RAR File";
+            this.btnRar.UseVisualStyleBackColor = true;
+            this.btnRar.Click += new System.EventHandler(this.btnRar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Man_in_the_mirror.Properties.Resources.hd_winrar_icon_by_rhubarb_leaf_d523xqo;
+            this.pictureBox1.Location = new System.Drawing.Point(551, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnDoIt
+            // 
+            this.btnDoIt.Enabled = false;
+            this.btnDoIt.Location = new System.Drawing.Point(309, 399);
+            this.btnDoIt.Name = "btnDoIt";
+            this.btnDoIt.Size = new System.Drawing.Size(191, 64);
+            this.btnDoIt.TabIndex = 4;
+            this.btnDoIt.Text = "Hide Rar";
+            this.btnDoIt.UseVisualStyleBackColor = true;
+            this.btnDoIt.Click += new System.EventHandler(this.btnDoIt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "not ready";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "not ready";
+            // 
+            // ofdRar
+            // 
+            this.ofdRar.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Image Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(562, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "RAR Path";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(276, 489);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Image Location:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDoIt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRar);
+            this.Controls.Add(this.imgTogo);
+            this.Controls.Add(this.btnImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgTogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.OpenFileDialog ofdImage;
+        private System.Windows.Forms.PictureBox imgTogo;
+        private System.Windows.Forms.Button btnRar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnDoIt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog ofdRar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Man in the mirror/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Man_in_the_mirror
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if(label1.Text=="Ready!" && label2.Text == "Ready!")
+            {
+                btnDoIt.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CriaPasta();
+
+            if (label1.Text == "Ready!" && label2.Text == "Ready!")
+            {
+                btnDoIt.Enabled = true;
+            }
+
+        }
+
+        private void CriaPasta()
+        {
+            if (!System.IO.Directory.Exists(folderName))
+            {
+                System.IO.Directory.CreateDirectory(folderName);
+            }
+            else
+            {
+                System.Console.Write("yep");
+            }
+
+
+        }
+
+        string folderName = @"C:\Users\Deus\Desktop\Copia";
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ofdImage.ShowDialog() == DialogResult.OK)
+            {
+                fileImg = ofdImage.FileName;
+
+                imgTogo.Image=new Bitmap(ofdImage.FileName);
+                label1.Text = "Ready!";
+                label3.Text = ofdImage.FileName;
+
+            }
+
+            if (label1.Text == "Ready!" && label2.Text == "Ready!")
+            {
+                btnDoIt.Enabled = true;
+            }
+
+
+        }
+
+        private void btnRar_Click(object sender, EventArgs e)
+        {
+            if (ofdRar.ShowDialog() == DialogResult.OK)
+            {
+
+                fileRar = ofdRar.FileName;
+                label4.Text = ofdRar.FileName;
+
+                label2.Text = "Ready!";
+
+            }
+
+            if (label1.Text == "Ready!" && label2.Text == "Ready!")
+            {
+                btnDoIt.Enabled = true;
+            }
+
+
+        }
+        string fileImg, fileRar;
+
+        private void btnDoIt_Click(object sender, EventArgs e)
+        {
+
+            cmdCopy(fileImg, fileRar);
+
+            //ofdImage.fil
+
+        }
+
+
+        private void cmdCopy(string img, string rar)
+        {
+            string command = "copy / b "+img+" + "+rar +" "+ @"C:\Users\Deus\Desktop\Copia\thingshidded.jpg";
+                   
+            string strCmdText;
+
+            strCmdText = "/C "+command;
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+            label5.Text = label5.Text+@"C:\Users\Deus\Desktop\Copia";
+
+        }
+
+
+    }
+
+
+
+    
+
+}
